e08cb0c5c81cb2c8ffacae01293e60fcc1c29163
cjj01101 <cjj01101@163.com>
2022-02-22 19:03:51 +0800

    complete leave message script

--- scripts/leave_message.php
+++ scripts/leave_message.php
@@ -1,1 +1,23 @@
-欢迎<?php echo $_POST["name"]; ?>!
+<?php
+
+$name=$_POST["name"];
+$content=$_POST["message"];
+
+$file="../data/message.xml";
+$doc = new DOMDocument();
+$doc->load($file);
+
+$root=$doc->documentElement;
+$message=$doc->createElement("message");
+$root->appendChild($message);
+
+$mname=$doc->createElement("name",$name);
+$mcont=$doc->createElement("content",$content);
+$message->appendChild($mname);
+$message->appendChild($mcont);
+
+$doc->save($file);
+
+echo "欢迎".$name."！留言成功，内容为：".$content;
+
+?>
